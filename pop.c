cf9c4794ba9be47be66e7733d7205c0534883ed2
osunx <splendorfreeman1@gmail.com>
2023-11-16 20:22:29 +0000

    Stack and queues - LIFO, FIFO

--- pop.c
+++ pop.c
@@ -8,24 +8,27 @@
  */
 void pop(stack_t **head, unsigned int counter)
 {
-	stack_t *topNode;
+        stack_t *topNode;
 
-	/* Check if the stack is empty */
-	if (*head == NULL)
-	{
-		fprintf(stderr, "L%d: can't pop an empty stack\n", counter);
-		fclose(mover.file);
-		free(mover.content);
-		freestack(*head);
-		exit(EXIT_FAILURE);
-	}
+        /* Check if the stack is empty */
+        if (*head == NULL)
+        {
+                fprintf(stderr, "L%d: can't pop an empty stack\n", counter);
+                fclose(mover.file);
+                free(mover.content);
+                freestack(*head);
+                exit(EXIT_FAILURE);
+        }
 
-	/* Save the top node */
-	topNode = *head;
+        /* Save the top node */
+        topNode = *head;
 
-	/* Update the head of the stack to the next node */
-	*head = topNode->next;
+        /* Update the head of the stack to the next node */
+        *head = topNode->next;
 
-	/* Free the memory of the top node */
-	free(topNode);
+        /* Free the memory of the top node */
+        free(topNode);
+
+        /* Debugging output */
+        printf("Popped value: %d\n", topNode->n);
 }
